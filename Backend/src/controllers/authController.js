f665b8d98dc3b543082b7f48f9ba2cd2dfbc2999
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-08-16 21:02:34 +0545

    auth controller created

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,2 +1,29 @@
 const user = require("../models/user.model");
 
+const registerUser = async(req, res)=>{
+
+    const {name, dateOfBirth, location, email, password } = req.body;
+
+    if(!name || !dateOfBirth || !location || !email || !password){
+        return res.status(400).json({message:"All fields are required"});
+    }
+
+    const userExist = await user.findOne({email});
+
+    if(userExist){
+        return res.status(400).json({message:"User already exists, try to login"});
+    }
+
+    const newUser = await user.create({
+        name: name,
+        dateOfBirth: dateOfBirth,
+        location: location,
+        email: email,
+        password: password
+    })
+
+    return res.status(201).json(newUser);
+}
+
+module.exports = {registerUser};
+
