dd2da72201858dfcfc891b2bd0fff8d44a449d2d
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-08-30 17:36:08 +0545

    feat: add transaction and ledger models with validation and prevention of ledger modification

---
+++ Backend/src/models/ledger.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const ledgerModel = await mongoose.Schema({
+    account:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:[true, "Account is required for ledger creation"],
+        immutable:true
+    },
+    amount:{
+        type:Number,
+        required:[true, "Amount is required for ledger creation"],
+        immutable: true
+    },
+    transaction:{
+        type: String,
+        ref:"transaction",
+        required:[true,"Transaction is required for ledger creation"],
+        immutable:true
+    },
+    type:{
+        type: String,
+        enum:{
+            values:["CREDIT", "DEBIT"],
+            message:"Type must be either CREDIT or DEBIT"
+        },
+        required:[true,"Ledger type is required"],
+        immutable:true
+    }
+})
+
+ledgerModel.index({account: 1, transaction: 1});
+
+function preventLedgerModification(){
+    throw new Error("Ledger cannot be modified");
+}
+
+ledgerSchema.pre("findOneAndUpdate", preventLedgerModification);
+ledgerSchema.pre("findOneAndDelete", preventLedgerModification);
+ledgerSchema.pre("updateOne", preventLedgerModification);
+ledgerSchema.pre("deleteOne", preventLedgerModification);
+ledgerSchema.pre("remove", preventLedgerModification);
+ledgerSchema.pre("deleteMany", preventLedgerModification);
+ledgerSchema.pre("updateMany", preventLedgerModification);
+ledgerSchema.pre("findOneAndReplace", preventLedgerModification);
+
+const ledgerModel = mongoose.model("ledger", ledgerSchema);
+
+module.exports = ledgerModel;
---
+++ Backend/src/models/transaction.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = await mongoose.Schema({
+
+    fromAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:[true, "From Account is required for transaction creation"],
+        ref: "account"
+    },
+    toAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:[true, "To Account is required for transaction creation"],
+        ref: "account"
+    },
+    status:{
+        type: String,
+        enum:{
+            values:["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+            message:"Status must be either PENDING, COMPLETED, FAILED or REVERSED"
+        },
+        default:"PENDING"
+    },
+    amount:{
+        type: Number,
+        required:[true, "Amount is required for transaction creation"],
+        min:[0, "Amount must be greater than 0"]
+    },
+    idempotencyKey:{
+        type: Number,
+        required:[true, "Idempotency Key is required for transaction creation"],
+        unique: true
+    }
+})
+
+transactionSchema.index({fromAccount: 1, toAccount: 1});
+
+const transactionModel = mongoose.model("transaction", transactionSchema);
+
+module.exports = transactionModel;
